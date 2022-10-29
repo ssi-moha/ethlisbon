c67a8ba55bf6a2c4305de58334a36d190d785da1
Sinane <sin.sim@outlook.fr>
2022-10-29 11:01:22 +0100

    feat: setup apollo client

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,8 @@ const withTM = require('next-transpile-modules');
 
 module.exports = withTM(['ui', 'alchemy', 'domains', 'infra', 'supabase', 'pure'])({
   reactStrictMode: true,
+  env: {
+    HASURA_API_URL: process.env.HASURA_API_URL,
+    HASURA_API_KEY: process.env.HASURA_API_KEY,
+  },
 });
