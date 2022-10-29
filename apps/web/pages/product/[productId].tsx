4421e3ec06774ca941bc005df6c06fec8f3b4523
Malek Rezgui <m.rezgui@outlook.fr>
2022-10-29 12:56:47 +0100

    feat(ui): add product details

---
+++ apps/web/pages/product/[productId].tsx
@@ -0,0 +1,9 @@
+import { Box, ProductDetails } from 'ui';
+
+export default function Web() {
+  return (
+    <Box w="full" maxW="7xl" mx="auto">
+      <ProductDetails />
+    </Box>
+  );
+}
--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,6 +2,7 @@ export * from './ProductList';
 
 export * from './theme';
 export * from './Layout';
+export * from './ProductDetails/ProductDetails';
 
 // @chakra-ui
 export { Box, Flex } from '@chakra-ui/react';
