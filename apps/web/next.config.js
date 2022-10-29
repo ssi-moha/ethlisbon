5122a2f4e90f95a604703db577f893ebd1008dbe
Sinane <sin.sim@outlook.fr>
2022-10-29 11:33:40 +0100

    feat: apollo provider

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 const withTM = require('next-transpile-modules');
 
-module.exports = withTM(['ui', 'alchemy', 'domains', 'infra', 'supabase', 'pure'])({
+module.exports = withTM(['ui', 'alchemy', 'domains', 'infra', 'supabase', 'pure', 'apollo'])({
   reactStrictMode: true,
   env: {
     HASURA_API_URL: process.env.HASURA_API_URL,
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,14 +1,19 @@
 import '../min.css';
 import { AppProps } from 'next/app';
 import { Layout, ThemeProvider } from 'ui';
+import { useApollo, ApolloProvider } from 'apollo';
 
 function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
+
   return (
-    <ThemeProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
